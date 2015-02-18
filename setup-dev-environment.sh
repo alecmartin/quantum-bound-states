91ec86ca104ab628a1679b92d2099add1bafaa9c
Alec Martin <alec@alecmartin.com>
2015-02-17 22:04:11 -0700

    added downloader script for PhET dependencies

---
+++ setup-dev-environment.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# Download PhET dependencies
+git clone https://github.com/phetsims/example-sim.git ..
+git clone https://github.com/phetsims/assert.git ..
+git clone https://github.com/phetsims/axon.git ..
+git clone https://github.com/phetsims/brand.git ..
+git clone https://github.com/phetsims/chipper.git ..
+git clone https://github.com/phetsims/dot.git ..
+git clone https://github.com/phetsims/joist.git ..
+git clone https://github.com/phetsims/kite.git ..
+git clone https://github.com/phetsims/phet-core.git ..
+git clone https://github.com/phetsims/phetcommon.git ..
+git clone https://github.com/phetsims/scenery.git ..
+git clone https://github.com/phetsims/scenery-phet.git ..
+git clone https://github.com/phetsims/sun.git ..
+git clone https://github.com/phetsims/sherpa.git ..
