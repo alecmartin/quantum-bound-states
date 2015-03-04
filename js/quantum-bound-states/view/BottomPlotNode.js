828e0c078b3df4163d5402562f37a374764fdb79
untra <untra.sam@gmail.com>
2015-03-04 14:29:19 -0700

    bottom plot added

---
+++ js/quantum-bound-states/view/BottomPlotNode.js
@@ -0,0 +1,25 @@
+//  Copyright 2002-2014, University of Colorado Boulder
+
+/**
+ *
+ * @author Emily Randall
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
+
+  function BottomPlotNode( model, options ) {
+
+    Node.call( this, options );
+
+    var background = new Rectangle(0,0,300,200,0,0, {fill:'black'});
+
+    
+  }
+
+  return inherit( Node, BottomPlotNode);
+} );
