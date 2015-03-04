3550d9f204a367eea5eae0c5d807cb44bca0fa87
untra <untra.sam@gmail.com>
2015-03-04 15:28:43 -0700

    vertical gray lines lookin radtastic

--- js/quantum-bound-states/view/BottomPlotNode.js
+++ js/quantum-bound-states/view/BottomPlotNode.js
@@ -9,8 +9,11 @@ define( function( require ) {
 
   // modules
   var inherit = require( 'PHET_CORE/inherit' );
+  var Line = require( 'SCENERY/nodes/Line' );
   var Node = require( 'SCENERY/nodes/Node' );
+  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
+  var Text = require( 'SCENERY/nodes/Text' );
 
   function BottomPlotNode( model, width, height, options ) {
 
@@ -19,6 +22,16 @@ define( function( require ) {
     var background = new Rectangle(50,0,width,height,0,0, {fill:'black'});
     this.addChild( background );
     
+    var divisors = (model.maxX - model.minX);
+    var xSpacing = (width  / divisors);
+    var xLoc = xSpacing /2;
+    for (var i = model.minX + 0.5; i < divisors + model.minX; i += 1) {
+      var tick = new Line(background.left + xLoc, background.top, background.left+xLoc, background.bottom, {stroke: 'gray'});
+      this.addChild( tick );
+      this.addChild( new Text( i.toString(), {centerX: tick.centerX, top: background.bottom + 5, font: new PhetFont( 12 )} ) );
+      xLoc += xSpacing;
+    }
+
   }
 
   return inherit( Node, BottomPlotNode);
