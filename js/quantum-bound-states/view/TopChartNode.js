5971a4b819ae9a85dd6bc1f8da33837c1f8bd24b
Emily Randall <emilymrandall@comcast.net>
2015-03-07 14:08:22 -0700

    topchart

---
+++ js/quantum-bound-states/view/TopChartNode.js
@@ -0,0 +1,17 @@
+//  Copyright 2002-2014, University of Colorado Boulder
+
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
+  var Text = require( 'SCENERY/nodes/Text' );
+
+  function TopChartNode( model, options ) {
+    Node.call( this, options );
+  }
+
+  return inherit( Node, TopChartNode);
+} );
