b7c89364258c6d766b986d5dda346a78db98fdd1
untra <untra.sam@gmail.com>
2015-03-07 14:44:07 -0700

    a few changes

--- js/quantum-bound-states/view/TopChartNode.js
+++ js/quantum-bound-states/view/TopChartNode.js
@@ -8,10 +8,47 @@ define( function( require ) {
   var Node = require( 'SCENERY/nodes/Node' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
   var Text = require( 'SCENERY/nodes/Text' );
+  var HBox = require( 'SCENERY/nodes/HBox' );
+  var HStrut = require( 'SUN/HStrut' );
+  var Panel = require( 'SUN/Panel' );
+  var VerticalCheckBoxGroup = require( 'SUN/VerticalCheckBoxGroup' );
+  var VBox = require( 'SCENERY/nodes/VBox' );
+
+
+
+  // Strings and other variables
+  var OptionFont = {font: new PhetFont( 12 )};
+  var magnifyingString = require( 'string!QUANTUM_BOUND_STATES/top-chart-show-magnifying-glass' );
+
 
   function TopChartNode( model, options ) {
     Node.call( this, options );
   }
 
+  // Add the control panel that will allow users to control the visibility
+  // of the plate charges and electric field lines
+  var magnifyingCheckBoxGroup = new VerticalCheckBoxGroup( [
+    { content: new Text( magnifyingString, OptionFont ), property: capacitorLabModel.plateChargeVisibleProperty, label: magnifyingString },
+  ], { boxWidth: 15, spacing: 5 } );
+  var titleToControlsVerticalSpace = 7;
+  var viewVisibilityControlsVBox = new VBox( {
+    children: [
+      new Text( viewTitleString, titleFont ),
+      new VStrut( titleToControlsVerticalSpace ),
+      new HBox( { children: [ new HStrut( 10 ), magnifyingCheckBoxGroup, new HStrut( 15 ) ] } )
+    ],
+    align: 'left'
+  } );
+  var viewVisibilityControlPanel = new Panel( viewVisibilityControlsVBox,
+  {
+    xMargin: 5,
+    fill: 'rgb( 0, 0, 0 )',
+    top: 5,
+    right: this.layoutBounds.width - 10,
+  } );
+  this.addChild( viewVisibilityControlPanel );
+
   return inherit( Node, TopChartNode);
+
+
 } );
