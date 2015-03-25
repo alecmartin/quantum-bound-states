646614e012ce2d662c48f5a24c566702646ae294
untra <untra.sam@gmail.com>
2015-03-25 17:02:10 -0600

    commented out a few lines in screenview

--- js/quantum-bound-states/view/EnergyLine.js
+++ js/quantum-bound-states/view/EnergyLine.js
@@ -8,15 +8,15 @@ define( function( require ){
   
   var mouse = require( 'SCENERY/input/Mouse' );
 
-  function EnergyLine( width, options ){
+  function EnergyLine( model, width, height, options ){
 	  
-    Line.call(this, options);
-    var x1 = 0;
-    var y1 = 0;
-    var y2 = 0;
+    // Line.call(this, options);
+    // var x1 = 0;
+    // var y1 = 0;
+    // var y2 = 0;
   
-    var line = new Line(x1, y1, width, y2, {fill:'green'});
-    this.addChild(line);
+    // var line = new Line(x1, y1, width, y2, {fill:'green'});
+    // this.addChild(line);
   
    //  while(mouse.over(width, this)){
 	  // var line = new Line(x1, y1, width, y2, {fill:'red'});
