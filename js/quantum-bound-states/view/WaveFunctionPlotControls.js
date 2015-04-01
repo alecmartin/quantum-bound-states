9a2449eca4cc379ae7926d04ebc94f4bea0a7c4b
Alec Martin <alec@alecmartin.com>
2015-04-01 16:54:56 -0400

    Checkbox disable on radio button working, text color change not working

--- js/quantum-bound-states/view/WaveFunctionPlotControls.js
+++ js/quantum-bound-states/view/WaveFunctionPlotControls.js
@@ -48,47 +48,49 @@ define( function( require ) {
     // Layout values
     var radioButtonFont = {font: new PhetFont( 14 ), fill: "palegoldenrod"};
     var checkBoxFontEnabled = {font: new PhetFont( 12 ), fill: "white"};
-    var checkBoxFontDisabled = {font: new PhetFont( 12 ), fill: "gray"};
+    var checkBoxFontDisabled = {font: new PhetFont( 12 ), fill: "grey"};
     var boxwidth = 240;
  
     // Radio buttons, check boxes, and associated logic
-    var checkBoxesEnabledProperty = new Property( false );
-    
     var radioButtonContent = [
-      { property: model.showProbDensityProperty && checkBoxesEnabledProperty, 
+      { property: model.showProbDensityProperty,
         value: true, 
         node: new Text( probabilityDensityString, radioButtonFont )
       },
-      { property: model.showProbDensityProperty && checkBoxesEnabledProperty, 
+      { property: model.showProbDensityProperty,
         value: false, 
         node: new Text( waveFunctionString, radioButtonFont )
       },
     ];
 
-    
+    var realPartText =  new Text( realPartString, checkBoxFontDisabled );
+    var imaginaryPartText =  new Text( imaginaryPartString, checkBoxFontDisabled );
+    var magnitudeText =  new Text( magnitudeString, checkBoxFontDisabled );
+    var phaseText =  new Text( phaseString, checkBoxFontDisabled );
+
     var checkBoxContent = [
-      { content: new HBox( { children: [ new Text( realPartString, checkBoxFontEnabled ),
+      { content: new HBox( { children: [ realPartText, 
                                          new HStrut( 15 ),
                                          new Line( 0, 0, 20, 0, { stroke: 'orange', lineWidth: 3 } )
                                        ] } ),
         property: model.showRealProperty, 
         label: realPartString, 
       },
-      { content: new HBox( { children: [ new Text( imaginaryPartString, checkBoxFontEnabled ),
+      { content: new HBox( { children: [ imaginaryPartText,
                                          new HStrut( 15 ),
                                          new Line( 0, 0, 20, 0, { stroke: 'blue', lineWidth: 3 } )
                                        ] } ),
         property: model.showImaginaryProperty, 
         label: imaginaryPartString 
       },
-      { content: new HBox( { children: [ new Text( magnitudeString, checkBoxFontEnabled ), 
+      { content: new HBox( { children: [ magnitudeText,
                                          new HStrut( 15 ),
                                          new Line( 0, 0, 20, 0, { stroke: 'white', lineWidth: 3 } )
                                        ] } ),
         property: model.showMagnitudeProperty, 
         label: magnitudeString 
       },
-      { content: new HBox( { children: [ new Text( phaseString, checkBoxFontEnabled ),
+      { content: new HBox( { children: [ phaseText,
                                          new HStrut( 15 ),
                                          new Image( 'images/PhaseIcon.png', { left: 0, top: 0, scale: 0.25 } )
                                        ] } ),
@@ -105,19 +107,20 @@ define( function( require ) {
                                                    } );
 
     // Make checkboxes disabled when Probability Density button is selected
-    checkBoxesEnabledProperty.link( function( isEnabled ) {
-      for(var i = 0; i < 4; i++) {
+    model.showProbDensityProperty.link( function( probDensitySelected ) {
+      for(var i = 0; i < checkBoxGroup.children.length; i++) {
         // Disable checkbox: the children of checkBoxGroup are HBoxes. Each HBox has a children array
         // containing the checkbox we need to manipulate as its only element.
-        checkBoxGroup.children[i].children[0].enabled = isEnabled;
+        checkBoxGroup.children[i].children[0].enabled = !probDensitySelected;
 
-        // Grey out type: each checkbox has a content value, which has a children array, the 0th element of
-        // which is text.
-        if( isEnabled) {
+        // Grey out type:
+        if( probDensitySelected ) {
           // Make text white
+          checkBoxGroup.children[i].children[0].content._children[0].options.children[0]._fill ='grey';
         }
         else {
           //Make text grey
+          checkBoxGroup.children[i].children[0].content._children[0].options.children[0]._fill ='white';
         }
       }
     } );
